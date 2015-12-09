d77ac9b346b00cd28f6229f25f1aa3ec41d5de7c
shuchangbruce <shuchangbruce@gmail.com>
2015-12-09 05:02:36 -0800

    Revert "contact form"
    This reverts commit 5e22459e83dca7325de706b530ef71ed0e559850.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('djApp', ['ui.router','firebase'])
+angular.module('djApp', ['ui.router'])
     .config(function($stateProvider, $urlRouterProvider) {
         'use strict'; //strict mode
 
@@ -27,30 +27,10 @@ angular.module('djApp', ['ui.router','firebase'])
         $urlRouterProvider.otherwise('/');
     })
     .controller('homeController', function() {
-        console.log('hahaha home ');
         //do nothing
     })
-    .controller('contactController', function($window, $scope, $timeout) {
-        $scope.submit = false;
-        $scope.saveMessage = function() {
-            var rootRef = new Firebase('https://djbreezee.firebaseio.com/').child('message');
-            var latestMessage = new Firebase('https://djbreezee.firebaseio.com/').child('message').child('latest');
-            var newContact = {
-                "first":$scope.contact.fname,
-                "last": $scope.contact.lname,
-                "email": $scope.contact.email,
-                "message": $scope.contact.message,
-                "createdAt": Firebase.ServerValue.TIMESTAMP
-            };
-            var push = rootRef.push(newContact);
-            var newId = push.key();
-            newContact.idNumber = newId;
-            latestMessage.update(newContact);
-            $scope.contact=null;
-            $scope.contactForm.$setPristine();
-            $scope.submit = true;
-            $timeout(function(){
-                $scope.submit = false;
-            }, 5000);
-        }
+    .controller('contactController', function($window, $scope) {
+
+
+
     })
