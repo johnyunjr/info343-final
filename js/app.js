5e22459e83dca7325de706b530ef71ed0e559850
shuchangbruce <shuchangbruce@gmail.com>
2015-12-09 05:02:12 -0800

    contact form

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('djApp', ['ui.router'])
+angular.module('djApp', ['ui.router','firebase'])
     .config(function($stateProvider, $urlRouterProvider) {
         'use strict'; //strict mode
 
@@ -27,10 +27,30 @@ angular.module('djApp', ['ui.router'])
         $urlRouterProvider.otherwise('/');
     })
     .controller('homeController', function() {
+        console.log('hahaha home ');
         //do nothing
     })
-    .controller('contactController', function($window, $scope) {
-
-
-
+    .controller('contactController', function($window, $scope, $timeout) {
+        $scope.submit = false;
+        $scope.saveMessage = function() {
+            var rootRef = new Firebase('https://djbreezee.firebaseio.com/').child('message');
+            var latestMessage = new Firebase('https://djbreezee.firebaseio.com/').child('message').child('latest');
+            var newContact = {
+                "first":$scope.contact.fname,
+                "last": $scope.contact.lname,
+                "email": $scope.contact.email,
+                "message": $scope.contact.message,
+                "createdAt": Firebase.ServerValue.TIMESTAMP
+            };
+            var push = rootRef.push(newContact);
+            var newId = push.key();
+            newContact.idNumber = newId;
+            latestMessage.update(newContact);
+            $scope.contact=null;
+            $scope.contactForm.$setPristine();
+            $scope.submit = true;
+            $timeout(function(){
+                $scope.submit = false;
+            }, 5000);
+        }
     })
